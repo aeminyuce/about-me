17fefb284f8eefbdef6f579d0aa1c25980968011
emin <yuce_ahmet@hotmail.com>
2025-10-13 23:20:15 +0300

    update page loader

--- src/views/PageLoader.tsx
+++ src/views/PageLoader.tsx
@@ -3,5 +3,5 @@ import Loadingmask from 'uilab/react/Loadingmask';
 
 export default function PageLoader() {
     Loadingmask('#page-loader');
-    return <div id='page-loader' className='ui-set-fixed ui-set-all'></div>;
+    return <div id='page-loader' className='ui-set-fixed'></div>;
 }
