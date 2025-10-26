39514acbf55c0ca1b723ecdc59b8f81cacee3a46
emin <yuce_ahmet@hotmail.com>
2025-10-26 20:35:35 +0300

    update about me

--- src/views/AboutMe.tsx
+++ src/views/AboutMe.tsx
@@ -14,17 +14,19 @@ import { useStoreContext } from '../states/StoreContext';
 import { IconMarker } from 'uilab-icons/react/general/marker';
 
 export default function () {
-    return (
+    const { apiResponse } = useStoreContext();
+
+    return apiResponse?.aboutMe && (
         <>
         <Profile />
-        <PersonalSkills />
+        {apiResponse?.aboutMe?.personalSkills && <PersonalSkills />}
         </>
-    );
+    )
 }
 
 const GetInTouchButtons = () => {
     const { apiResponse } = useStoreContext();
-    const { getInTouchText } = apiResponse?.aboutMe;
+    const getInTouchText = apiResponse?.aboutMe?.getInTouchText;
 
     return (
         <div className='ui-form-lg ui-ease-1st-btn'>
@@ -40,25 +42,44 @@ const GetInTouchButtons = () => {
 
 const Profile = () => {
     const { apiResponse } = useStoreContext();
-    const { profileImage, fullName, jobTitle, location } = apiResponse?.aboutMe;
+    const aboutMe = apiResponse?.aboutMe;
+
+    const profileImage = aboutMe?.profileImage;
+    const fullName = aboutMe?.fullName;
+    const jobTitle = aboutMe?.jobTitle;
+    const location = aboutMe?.location;
 
     return (
         <Grid.Row fluid='xs' hGap='lg' className='ui-m-30-t ui-m-5-b'>
             <Grid.Col size={5} className='ui-align-r ui-xs-align-c'>
-                <Avatar size='lg' className='ui-circle'>
-                    <img src={profileImage} alt={`${fullName} | ${jobTitle}`} />
-                </Avatar>
+                {profileImage &&
+                    <Avatar size='lg' className='ui-circle'>
+                        <img src={profileImage} alt={`${fullName || ''}${jobTitle ? ` | ${jobTitle}` : ''}`} />
+                    </Avatar>
+                }
             </Grid.Col>
             <Grid.Col size={7} className='ui-xs-align-c'>
-                <h1 className='ui-h1 ui-font-30 ui-font-bold ui-m-15-t'>{fullName}</h1>
-                <span className='ui-font-16 ui-m-5-b ui-block'>{jobTitle}</span>
+                {fullName &&
+                    <h1 className='ui-h1 ui-font-30 ui-font-bold ui-m-15-t'>{fullName}</h1>
+                }
 
-                <SvgIcon as='js' src={IconMarker} size='sm' className='ui-m-5-r' />
-                <span className='ui-font-16 ui-inline-block'>{location}</span>
+                {jobTitle &&
+                    <span className='ui-font-16 ui-m-5-b ui-block'>{jobTitle}</span>
+                }
 
-                <Spacer size={25} />
+                {location &&
+                    <>
+                    <SvgIcon as='js' src={IconMarker} size='sm' className='ui-m-5-r' />
+                    <span className='ui-font-16 ui-inline-block'>{location}</span>
+                    </>
+                }
 
-                <GetInTouchButtons />
+                {aboutMe?.getInTouchText &&
+                    <>
+                    <Spacer size={25} />
+                    <GetInTouchButtons />
+                    </>
+                }
             </Grid.Col>
         </Grid.Row>
     )
@@ -66,14 +87,13 @@ const Profile = () => {
 
 const PersonalSkills = () => {
     const { apiResponse } = useStoreContext();
-    const { personalSkills } = apiResponse?.aboutMe;
 
     return (
         <Grid.Row>
             <Grid.Col size={6} push={3} className='ui-m-30-b'>
                 <p className='ui-color-black-25 ui-font-15 ui-align-c'>
 
-                    {personalSkills.map((name: string, index: number, arr: string[]) => {
+                    {apiResponse?.aboutMe?.personalSkills.map((name: string, index: number, arr: string[]) => {
                         const item = `${name}${index + 1 !== arr.length ? ', ' : ''}`;
                         return <Fragment key={name}>{item}</Fragment>
                     })}
