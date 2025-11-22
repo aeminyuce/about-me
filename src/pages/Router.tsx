d927856434ef818924fbb2720611261b6e042197
aeminyuce <yuce_ahmet@hotmail.com>
2025-11-23 00:34:36 +0300

    move error pages from view

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -5,8 +5,8 @@ import { Routes, Route } from 'react-router-dom';
 // misc
 import PageTitle from '../views/PageTitle';
 import PageLoader from '../views/PageLoader';
-import Page500 from '../views/500';
-import Page404 from '../views/404';
+import Page500 from '../pages/500';
+import Page404 from '../pages/404';
 
 // routes
 const Home = lazy(() => import( /* webpackChunkName: "Home" */ "./Home" ));
