281cf4be5b5aa4204b20de7dd1af70e53501c1dc
aeminyuce <yuce_ahmet@hotmail.com>
2026-01-02 00:20:33 +0300

    remove missing import

--- src/pages/Lab.tsx
+++ src/pages/Lab.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useOutlet, Outlet } from "react-router-dom";
+import { useOutlet } from "react-router-dom";
 import Grid from 'uilab/react/Grid';
 
 // misc
