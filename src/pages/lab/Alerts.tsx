db7053f0e4debfa6181adbd8722f35ff7e46e982
aeminyuce <yuce_ahmet@hotmail.com>
2026-01-04 22:23:44 +0300

    update alerts page

--- src/pages/lab/Alerts.tsx
+++ src/pages/lab/Alerts.tsx
@@ -1,14 +1,22 @@
 import * as React from 'react';
-
-// misc
-import { useStoreContext } from '../../states/StoreContext';
+import Code from 'uilab/react/Code';
 
 export default function () {
-    const { apiResponse } = useStoreContext();
-
     return (
         <>
-        alerts
+        <Code clipboard className='ui-round'>
+            <Code.Line type='comment' str="JS" />
+            <Code.Line str="return ui.alerts.dialog({" />
+            <Code.Line inset={1} str="msg: 'Submit this form?'," />
+            <Code.Line inset={1} str="success: 'Yes'," />
+            <Code.Line inset={1} str="error: 'No'," />
+            <Code.Line inset={1} str="callback: function (value) {" />
+            <Code.Line inset={2} str="if (value === 'success') {" />
+            <Code.Line inset={3} str="document.getElementById('exampleForm').submit();" />
+            <Code.Line inset={2} str="}" />
+            <Code.Line inset={1} str="}" />
+            <Code.Line str="});" />
+        </Code>
         </>
     );
 }
