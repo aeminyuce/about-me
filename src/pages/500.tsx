bcd67039adb85f24d36e0cadfda06379ddab0185
aeminyuce <yuce_ahmet@hotmail.com>
2026-01-06 23:48:54 +0300

    fix quotes

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -8,7 +8,7 @@ export default function () {
             <Heading as='h1' className='ui-font-72'>
                 500
             </Heading>
-            <p className="ui-m-30-b">Server error</p>
+            <p className='ui-m-30-b'>Server error</p>
             <Button className='ui-circle' to='/'>Go to Home</Button>
         </div>
     );
