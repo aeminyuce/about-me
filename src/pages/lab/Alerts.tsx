e173dd1ef048351d3566daee5123f6c4b9050d5e
aeminyuce <yuce_ahmet@hotmail.com>
2026-01-08 23:25:47 +0300

    update alerts page

--- src/pages/lab/Alerts.tsx
+++ src/pages/lab/Alerts.tsx
@@ -1,17 +1,85 @@
 import * as React from 'react';
+import Alerts from 'uilab/react/Alerts';
 import Button from 'uilab/react/Button';
 import Code from 'uilab/react/Code';
 import Grid from 'uilab/react/Grid';
 import Heading from 'uilab/react/Heading';
-import { ui } from 'uilab/js/core/globals';
+import Spacer from 'uilab/react/Spacer';
 
 export default function () {
     return (
         <>
         <Heading as='h1'>Alerts</Heading>
 
-        <Heading as='h3'>Showing Dialogues</Heading>
-        <Code className='ui-round'>
+        <Heading as='h3'>Dialog Messages</Heading>
+        <div className='ui-highlight ui-p-30 ui-md-no-p-h ui-round ui-border ui-border-light'>
+            <Grid.Row fluid='lg'>
+                <Grid.Col size={6}>
+
+                    <div className='ui-alerts-dialog ui-alerts-demo ui-round ui-shadow-lg ui-show ui-show-ease ui-m-30-t ui-m-auto ui-set-static'
+                        style={{ width: '65%', transform: 'none' }}>
+                        <div className='ui-dialog-msg'>Simple dialog message.</div>
+                        <div className='ui-dialog-buttons ui-ease-1st-btn'>
+                            <button className='ui-dialog-success' value='success'>OK</button>
+                        </div>
+                    </div>
+
+                    <Spacer size={30} className='ui-m-20-v' />
+
+                    <Code clipboard className='ui-m-5-b ui-m-10-t ui-m-15-h ui-round'>
+                        <Code.Comment>JS</Code.Comment>
+                        <Code.Lines>
+{`Alerts.Dialog({
+    msg: 'Simple dialog message.',
+});`}
+                        </Code.Lines>
+                    </Code>
+                    <Button size='sm' className='ui-m-15-l ui-round ui-theme-blue ui-fill-dark-100'
+                        onClick={() => {
+                            Alerts.Dialog({
+                                msg: 'Simple dialog message.',
+                            });
+                        }}>
+                        Run Code
+                    </Button>
+
+                </Grid.Col>
+                <Grid.Col size={6}>
+
+                    <div className='ui-alerts-dialog ui-alerts-demo ui-round ui-shadow-lg ui-show ui-show-ease ui-m-30-t ui-m-auto ui-set-static'
+                        style={{ width: '65%', transform: 'none' }}>
+                        <div className='ui-dialog-msg'>With custom success button text.</div>
+                        <div className='ui-dialog-buttons ui-ease-1st-btn'>
+                            <button className='ui-dialog-success' value='success'>Got it!</button>
+                        </div>
+                    </div>
+
+                    <Spacer size={30} className='ui-m-20-v' />
+
+                    <Code clipboard className='ui-m-5-b ui-m-10-t ui-m-15-h ui-round'>
+                        <Code.Comment>JS</Code.Comment>
+                        <Code.Lines>
+{`Alerts.Dialog({
+    msg: 'With custom success button text.',
+    success: 'Got it!',
+});`}
+                        </Code.Lines>
+                    </Code>
+                    <Button size='sm' className='ui-m-15-l ui-round ui-theme-blue ui-fill-dark-100'
+                        onClick={() => {
+                            Alerts.Dialog({
+                                msg: 'With custom success button text.',
+                                success: 'Got it!',
+                            });
+                        }}>
+                        Run Code
+                    </Button>
+
+                </Grid.Col>
+            </Grid.Row>
+        </div>
+
+        {/* <Code className='ui-round'>
             <Code.Comment>Usage</Code.Comment>
             <Code.Lines>
                 {`ui.alerts.dialog({ msg: '', success: '', error: '', custom: { value: 'button text', value: 'button text' }, callback });`}
@@ -95,7 +163,7 @@ export default function () {
             <Button type='submit' className='ui-round ui-theme-blue ui-fill-dark-100'>
                 Submit Form
             </Button>
-        </form>
+        </form> */}
         </>
     );
 }
