5af8b4bec9011a353d04f1e74ef527e6b2d670b1
aeminyuce <yuce_ahmet@hotmail.com>
2026-01-11 00:57:25 +0300

    add grid

--- src/pages/lab/Intro.tsx
+++ src/pages/lab/Intro.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Grid from 'uilab/react/Grid';
 
 // misc
 import { useStoreContext } from '../../states/StoreContext';
@@ -21,17 +22,21 @@ export default function() {
     const textPart3 = intro?.textPart3;
 
     return (
-        <div className='ui-align-c ui-p-30-v ui-md-no-p'>
-            <MyAvatar />
+        <Grid.Row>
+            <Grid.Col xl={{ size: 10, push: 1}} lg={{ size: 10, push: 1}} size={12} className='ui-align-c ui-p-30-v'>
 
-            {title && <Title />}
-            {subTitle && <SubTitle />}
-            {images && <IntroImages />}
+                <MyAvatar />
 
-            <div className='ui-m-30-h ui-sm-no-m'>
-                {leadText && <LeadText />}
-                {(textPart1 || textPart2 || textPart3) && <Paragraphs />}
-            </div>
-        </div>
+                {title && <Title />}
+                {subTitle && <SubTitle />}
+                {images && <IntroImages />}
+
+                <div className='ui-m-30-h ui-sm-no-m'>
+                    {leadText && <LeadText />}
+                    {(textPart1 || textPart2 || textPart3) && <Paragraphs />}
+                </div>
+
+            </Grid.Col>
+        </Grid.Row>
     )
 }
