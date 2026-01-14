16b1038aa603bc163cc6134b751835be68e8354f
aeminyuce <yuce_ahmet@hotmail.com>
2026-01-15 00:10:13 +0300

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -75,7 +75,7 @@ module.exports = (env, argv) => {
     }
     if (argv.mode === 'development') {
         config.devServer = {
-            host: 'localhost',
+            host: '0.0.0.0',
             port: 3000,
             open: true,
             historyApiFallback: true,
