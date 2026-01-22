6b343b4c296b96750990f4e2bf7a6ef73b89d219
aeminyuce <yuce_ahmet@hotmail.com>
2026-01-22 21:51:07 +0300

    modify lab breadcrumbs

--- src/components/lab/BreadcrumbsWrapper.tsx
+++ src/components/lab/BreadcrumbsWrapper.tsx
@@ -6,10 +6,10 @@ import type { BreadcrumbsWrapperProps } from '../../models/Lab';
 
 export default function (props: BreadcrumbsWrapperProps) {
     const { children } = props;
-    const classes = 'ui-opacity ui-ease-layout';
+    const classes = 'ui-opacity';
 
     return (
-        <Breadcrumbs className='ui-p-10-b ui-m-30-b ui-border-b ui-border-light'>
+        <Breadcrumbs className='ui-p-10-b ui-m-30-b ui-border-b ui-border-light ui-ease-2nd-layout'>
             <Breadcrumbs.Item to='/' className={classes}>Home</Breadcrumbs.Item>
             <Breadcrumbs.Item to='/lab' className={classes}>Lab</Breadcrumbs.Item>
             {children}
