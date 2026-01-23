a5549e2157adeb738aa948b7e63bca32b49a50ca
aeminyuce <yuce_ahmet@hotmail.com>
2026-01-24 00:59:06 +0300

    update home navigation

--- src/components/home/HomeNav.tsx
+++ src/components/home/HomeNav.tsx
@@ -3,17 +3,12 @@ import { useLocation } from 'react-router-dom';
 import Button from 'uilab/react/Button';
 import Carousel from 'uilab/react/Carousel';
 import Grid from 'uilab/react/Grid';
-import SvgIcon from 'uilab/react/SvgIcon';
 
 // misc
 import type { HomeNavLinksProps } from '../../models/Home';
 import ThemeChanger from './ThemeChanger';
 import { useStoreContext } from '../../states/StoreContext';
 import { mobileNavPosition } from '../../helpers/Home';
-
-// assets
-import { IconAngleLeft } from 'uilab-icons/react/general/angle-left';
-import { IconAngleRight } from 'uilab-icons/react/general/angle-right';
 
 export default function () {
     const { apiResponse } = useStoreContext();
@@ -22,21 +17,23 @@ export default function () {
     return navData && (
         <Grid.Row className='home-nav ui-p-15-b ui-m-15-b ui-border-b ui-border-light'>
             <Grid.Static fluid='no' className='ui-set-relative'>
-                <Grid.Col size={47} className='ui-visible-sm' />
                 <Grid.Row>
-                    <Grid.Col size={12} className='ui-ease-1st-btn ui-round-1st ui-hidden-sm'>
+                    <Grid.Col size={12}>
 
-                        {navData?.navLinks && <NavDesktopLinks />}
+                        <nav aria-label='Home page desktop navigation' className='ui-ease-1st-btn ui-round-1st ui-hidden-sm'>
+                            {navData?.navLinks && <NavDesktopLinks />}
+                        </nav>
 
                     </Grid.Col>
                     <Grid.Col size={12} className='ui-ease-1st-btn ui-visible-sm'>
 
-                        {navData?.navLinks && <NavMobileLinks />}
+                        <nav aria-label='Home page mobile navigation'>
+                            {navData?.navLinks && <NavMobileLinks />}
+                        </nav>
 
                     </Grid.Col>
                 </Grid.Row>
-                <Grid.Col size={42} className='ui-visible-sm' />
-                <Grid.Col size={69} className='ui-p-10-v'>
+                <Grid.Col size={70} className='ui-p-10-v'>
 
                     {navData?.themeList && <ThemeChanger />}
 
@@ -74,24 +71,16 @@ const NavMobileLinks = () => {
     const start = mobileNavPosition(navLinks, pathname);
 
     return (
-        <Carousel half start={start} sm={3} xs={2} className='ui-set-static ui-round ui-border ui-border-light'>
-            <Carousel.Nav className='ui-no-m'>
-                <Button ghost square noease className='ui-carousel-prev ui-m-2-t ui-round ui-set-absolute ui-set-l'>
-                    <SvgIcon as='js' toggle src={IconAngleLeft} />
-                </Button>
-                <Carousel.Dots className='ui-hidden' />
-                <Button ghost square noease className='ui-carousel-next ui-m-2-t ui-round ui-set-absolute ui-set-r'>
-                    <SvgIcon as='js' toggle src={IconAngleRight} />
-                </Button>
-            </Carousel.Nav>
-            <Carousel.Slider className='ui-m-2 ui-ease-2nd-btn ui-round-2nd'>
+        <Carousel start={start} sm={3} xs={2}>
+            <Carousel.Slider className='ui-ease-2nd-btn ui-round-2nd'>
 
                 {navLinks?.map((item: HomeNavLinksProps) => {
                     const selected = item.to === pathname;
+                    const classes = !selected ? 'ui-border ui-border-light' : null;
 
                     return (
-                        <Carousel.Content key={item.name} className='ui-p-2-r'>
-                            <Button ghost={!selected} block='all' noease to={item.to}>
+                        <Carousel.Content key={item.name} className='ui-p-3-r'>
+                            <Button noease ghost={!selected} block='all' to={item.to} className={classes}>
                                 {item.name}
                             </Button>
                         </Carousel.Content>
@@ -99,6 +88,9 @@ const NavMobileLinks = () => {
                 })}
 
             </Carousel.Slider>
+            <Carousel.Nav className='ui-hidden'>
+                <Carousel.Dots />
+            </Carousel.Nav>
         </Carousel>
     )
 }
