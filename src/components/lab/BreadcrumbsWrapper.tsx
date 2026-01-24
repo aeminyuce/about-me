ee86051f9c324849e92918db9cdff12798d1bcb9
aeminyuce <yuce_ahmet@hotmail.com>
2026-01-24 23:44:06 +0300

    create breadcrumbs page

--- src/components/lab/BreadcrumbsWrapper.tsx
+++ src/components/lab/BreadcrumbsWrapper.tsx
@@ -9,7 +9,7 @@ export default function (props: BreadcrumbsWrapperProps) {
     const classes = 'ui-opacity';
 
     return (
-        <Breadcrumbs className='ui-p-10-b ui-m-30-b ui-border-b ui-border-light ui-ease-2nd-layout'>
+        <Breadcrumbs sep='bull' className='ui-p-10-b ui-m-30-b ui-border-b ui-border-light ui-ease-2nd-layout'>
             <Breadcrumbs.Item pos={'1'} to='/' className={classes}>Home</Breadcrumbs.Item>
             <Breadcrumbs.Item pos={'2'} to='/lab' className={classes}>Lab</Breadcrumbs.Item>
             {children}
--- src/pages/lab/Breadcrumbs.tsx
+++ src/pages/lab/Breadcrumbs.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Breadcrumbs from 'uilab/react/Breadcrumbs';
 import Heading from 'uilab/react/Heading';
 import Spacer from 'uilab/react/Spacer';
+import SvgIcon from 'uilab/react/SvgIcon';
 
 // misc
 import Description from '../../components/common/Description';
@@ -9,13 +10,20 @@ import Preview from '../../components/common/Preview';
 import BreadcrumbsWrapper from '../../components/lab/BreadcrumbsWrapper';
 
 // assets
-import { IconUser } from 'uilab-icons/react/general/user';
+import { IconHome } from 'uilab-icons/react/general/home';
 
 export default function () {
-    const underlineWrapperClasses = 'ui-color-black-25 ui-font-hoverline-2nd';
+    // values
+    const item1 = 'Home';
+    const item2 = 'Search Results';
+    const item3 = 'Content Details';
 
-    const opacityWrapperClasses = 'ui-ease-2nd-layout';
-    const opacityClasses = 'ui-opacity';
+    // classes
+    const colorWrapperClasses = 'ui-color-black-25';
+
+    const underlineClasses = 'ui-font-hoverline';
+    const opacityClasses = 'ui-opacity ui-ease-layout';
+    const themeClasses = 'ui-text ui-theme-';
 
     return (
         <>
@@ -26,25 +34,76 @@ export default function () {
         <Heading as='h1'>Breadcrumbs</Heading>
 
         <Description>
-            Simple breadcrumb navigation with linked items separated by a bullet symbol. In the first variant, links show an underline on hover; in the second, links change opacity on hover.        </Description>
+            Simple breadcrumb navigation with linked items separated by different symbols. In the first variant group, links show an underline on hover except for the last item; in the second, links change opacity on hover.
+        </Description>
         <Preview>
-            <Breadcrumbs className={underlineWrapperClasses}>
-                <Breadcrumbs.Item pos={'1'} to='#'>Home</Breadcrumbs.Item>
-                <Breadcrumbs.Item pos={'2'} to='#'>Search Results</Breadcrumbs.Item>
-                <Breadcrumbs.Item pos={'3'}>
-                    <strong>Details</strong>
-                </Breadcrumbs.Item>
+            <Breadcrumbs sep='arrow' className={colorWrapperClasses}>
+                <Breadcrumbs.Item pos={'1'} to='#' className={underlineClasses}>{item1}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'2'} to='#' className={underlineClasses}>{item2}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'3'}>{item3}</Breadcrumbs.Item>
+            </Breadcrumbs>
+            <Breadcrumbs sep='bull' className={colorWrapperClasses}>
+                <Breadcrumbs.Item pos={'1'} to='#'>{item1}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'2'} to='#'>{item2}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'3'}>{item3}</Breadcrumbs.Item>
+            </Breadcrumbs>
+            <Breadcrumbs sep='pipe' className={colorWrapperClasses}>
+                <Breadcrumbs.Item pos={'1'} to='#' className={underlineClasses}>{item1}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'2'} to='#' className={underlineClasses}>{item2}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'3'}>{item3}</Breadcrumbs.Item>
             </Breadcrumbs>
 
             <Spacer size={15} />
 
-            <Breadcrumbs className={opacityWrapperClasses}>
-                <Breadcrumbs.Item pos={'1'} className={opacityClasses} to='#'>Home</Breadcrumbs.Item>
-                <Breadcrumbs.Item pos={'2'} className={opacityClasses} to='#'>Search Results</Breadcrumbs.Item>
-                <Breadcrumbs.Item pos={'3'}>Details</Breadcrumbs.Item>
+            <Breadcrumbs sep='arrow'>
+                <Breadcrumbs.Item pos={'1'} className={opacityClasses} to='#'>{item1}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'2'} className={opacityClasses} to='#'>{item2}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'3'}>{item3}</Breadcrumbs.Item>
+            </Breadcrumbs>
+            <Breadcrumbs sep='bull'>
+                <Breadcrumbs.Item pos={'1'} className={opacityClasses} to='#'>{item1}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'2'} className={opacityClasses} to='#'>{item2}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'3'}>{item3}</Breadcrumbs.Item>
+            </Breadcrumbs>
+            <Breadcrumbs sep='pipe'>
+                <Breadcrumbs.Item pos={'1'} className={opacityClasses} to='#'>{item1}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'2'} className={opacityClasses} to='#'>{item2}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'3'}>{item3}</Breadcrumbs.Item>
             </Breadcrumbs>
         </Preview>
 
+        <Description>
+            Breadcrumb navigation with the first item replaced by an icon. The second and third variants use custom theme colors, while the separators remain unstyled and do not adopt theme colors.
+        </Description>
+        <Preview>
+            <Breadcrumbs sep='arrow'>
+                <Breadcrumbs.Item pos={'1'} to='#' className={`${opacityClasses} ${underlineClasses}`}>
+                    <SvgIcon as='js' src={IconHome} />
+                </Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'2'} to='#' className={`${opacityClasses} ${underlineClasses}`}>{item2}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'3'}>{item3}</Breadcrumbs.Item>
+            </Breadcrumbs>
+
+            <Spacer size={15} />
+
+            <Breadcrumbs sep='bull' className={`${themeClasses}greenYellow`}>
+                <Breadcrumbs.Item pos={'1'} to='#' className={`${opacityClasses} ${underlineClasses}`}>
+                    <SvgIcon as='js' src={IconHome} />
+                </Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'2'} to='#' className={`${opacityClasses} ${underlineClasses}`}>{item2}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'3'}>{item3}</Breadcrumbs.Item>
+            </Breadcrumbs>
+
+            <Spacer size={15} />
+
+            <Breadcrumbs sep='pipe' className={`${themeClasses}blueGreen`}>
+                <Breadcrumbs.Item pos={'1'} to='#' className={`${opacityClasses} ${underlineClasses}`}>
+                    <SvgIcon as='js' src={IconHome} />
+                </Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'2'} to='#' className={`${opacityClasses} ${underlineClasses}`}>{item2}</Breadcrumbs.Item>
+                <Breadcrumbs.Item pos={'3'}>{item3}</Breadcrumbs.Item>
+            </Breadcrumbs>
+        </Preview>
         </>
     );
 }
