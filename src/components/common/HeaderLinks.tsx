897f6ae69d19b071538511af76e87a3ddeeb6895
aeminyuce <yuce_ahmet@hotmail.com>
2026-01-31 11:09:27 +0300

    update header links

--- src/components/common/HeaderLinks.tsx
+++ src/components/common/HeaderLinks.tsx
@@ -1,10 +1,14 @@
 import * as React from 'react';
 import Button from 'uilab/react/Button';
+import SvgIcon from 'uilab/react/SvgIcon';
 
 // misc
 import type { HeaderLinksProps } from '../../models/Page';
 import { ShowGetInTouch } from './GetInTouchModal';
 import { useStoreContext } from '../../states/StoreContext';
+
+// assets
+import { IconHeart } from 'uilab-icons/react/general/heart';
 
 export default function () {
     const { api } = useStoreContext();
@@ -19,6 +23,11 @@ export default function () {
 
                     return (
                         <Button key={item.name} ghost noease block='md' to={item.to} data={{ 'close': item.modal }} onClick={isModal} className='ui-m-2-r'>
+                            {item.to === '/icons' &&
+                                <>
+                                    I<SvgIcon as='js' src={IconHeart} size='xs' opacity='no' className='ui-m-5-h ui-m-4-v' />
+                                </>
+                            }
                             {item.name}
                         </Button>
                     )
