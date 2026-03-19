201c8cf04b47c7c3361d48ca6c5b7201d1b5b5bc
aeminyuce <yuce_ahmet@hotmail.com>
2026-03-20 01:06:42 +0300

    bug fix

--- src/components/icons/SizeChanger.tsx
+++ src/components/icons/SizeChanger.tsx
@@ -11,20 +11,22 @@ export default function () {
     const sizes = api?.icons?.sizes;
 
     return (
-        <Grid.Col size={4} offset={4}>
-            <Button.Wrapper as='div' type='holder' largeButtons ease='1st' className='ui-theme-redPurple'>
+        <Grid.Row>
+            <Grid.Col size={4} offset={4}>
+                <Button.Wrapper as='div' type='holder' largeButtons ease='1st' className='ui-theme-purpleBlue'>
 
-                {sizes?.map((item: SizeListProps) => {
-                    const classes = `ui-round${item.size === iconSize ? ' ui-fill-dark-100' : ''}`;
+                    {sizes?.map((item: SizeListProps) => {
+                        const classes = `ui-round${item.size === iconSize ? ' ui-fill-dark-100' : ''}`;
 
-                    return (
-                        <Button key={item.name} noease className={classes} data={{ 'size': item.size  }} onClick={() => setIconSize(item.size)}>
-                            {item.name}
-                        </Button>
-                    )
-                })}
+                        return (
+                            <Button key={item.name} noease className={classes} data={{ 'size': item.size  }} onClick={() => setIconSize(item.size)}>
+                                {item.name}
+                            </Button>
+                        )
+                    })}
 
-            </Button.Wrapper>
-        </Grid.Col>
+                </Button.Wrapper>
+            </Grid.Col>
+        </Grid.Row>
     )
 }
