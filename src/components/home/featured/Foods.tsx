48ec34b4f46a1c2a4a0a1c9a37fa7771e8817f45
aeminyuce <yuce_ahmet@hotmail.com>
2026-05-02 00:35:29 +0300

    add foods component

--- src/components/home/featured/Foods.tsx
+++ src/components/home/featured/Foods.tsx
@@ -18,40 +18,40 @@ import { IconForkKnife } from 'uilab-icons/react/kitchen/fork-knife';
 import { IconMitten } from 'uilab-icons/react/kitchen/mitten';
 
 export default function() {
-    const { themeB, api } = useStoreContext();
+    const { themeA, api } = useStoreContext();
     const foods = api?.home_featured?.foods;
 
     return (
-        <Card className='ui-p-10 ui-round ui-shadow-sm'>
-            <Card.Side className={`ui-color-black-50 ui-align-c ui-m-10-b ${themeB} ui-text`}>
+        <Card className={`ui-p-10 ui-round ui-shadow-sm ${themeA}`}>
+            <Card.Side className='ui-align-c ui-m-10-b'>
                 {foods?.food}
             </Card.Side>
             <Card.Side>
                 <Button.Wrapper type='holder' as='div' ease='1st' className='ui-m-1-b ui-round-t-1st ui-block-2nd ui-icons-no-opacity'>
                     <Button noease multi className='ui-p-10-v'>
-                        <SvgIcon as='js' src={IconChef} b={5} className='ui-m-auto' />
+                        <SvgIcon as='js' src={IconChef} b={5} className='ui-m-auto ui-text' />
                         {foods?.foodBtn1}
                     </Button>
                     <Button noease multi className='ui-p-10-v'>
-                        <SvgIcon as='js' src={IconPlate} b={5} className='ui-m-auto' />
+                        <SvgIcon as='js' src={IconPlate} b={5} className='ui-m-auto ui-text' />
                         {foods?.foodBtn2}
                     </Button>
                     <Button noease multi className='ui-p-10-v'>
-                        <SvgIcon as='js' src={IconCup} b={5} className='ui-m-auto' />
+                        <SvgIcon as='js' src={IconCup} b={5} className='ui-m-auto ui-text' />
                         {foods?.foodBtn3}
                     </Button>
                 </Button.Wrapper>
                 <Button.Wrapper type='holder' as='div' ease='1st' className='ui-round-b-1st ui-block-2nd ui-icons-no-opacity'>
                     <Button noease multi className='ui-p-10-v'>
-                        <SvgIcon as='js' src={IconCoffee} b={5} className='ui-m-auto' />
+                        <SvgIcon as='js' src={IconCoffee} b={5} className='ui-m-auto ui-text' />
                         {foods?.foodBtn4}
                     </Button>
                     <Button noease multi className='ui-p-10-v'>
-                        <SvgIcon as='js' src={IconForkKnife} b={5} className='ui-m-auto' />
+                        <SvgIcon as='js' src={IconForkKnife} b={5} className='ui-m-auto ui-text' />
                         {foods?.foodBtn5}
                     </Button>
                     <Button noease multi className='ui-p-10-v'>
-                        <SvgIcon as='js' src={IconMitten} b={5} className='ui-m-auto' />
+                        <SvgIcon as='js' src={IconMitten} b={5} className='ui-m-auto ui-text' />
                         {foods?.foodBtn6}
                     </Button>
                 </Button.Wrapper>
