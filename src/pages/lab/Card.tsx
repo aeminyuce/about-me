e2d809a807fc4e9ba9296768d696c15716018597
aeminyuce <yuce_ahmet@hotmail.com>
2026-05-03 01:32:45 +0300

    update card page

--- src/pages/lab/Card.tsx
+++ src/pages/lab/Card.tsx
@@ -55,15 +55,15 @@ export default function () {
                                 <Heading as='h2' className='ui-align-l'>{text?.forestTitle}</Heading>
                                 <p className='ui-font-18 ui-font-light ui-font-readable ui-align-l'>{text?.forest}</p>
                             </Card.Side>
-                            <Card.Side className='ui-set-relative'>
+                            <Card.Side className='ui-set-relative ui-theme-greenYellow'>
                                 <Image fluid src={img[0]} aspect='landscape' className='ui-round-b' />
-                                <div className='ui-p-30 ui-round ui-set-absolute ui-set-c ui-bg-blur'>
-                                    <p className='ui-color-white ui-font-18 ui-m-20-b'>
-                                        <SvgIcon as='js' src={IconTrees} size='xxl' />
-                                        <Spacer size={15} />
+                                <div className='ui-p-20 ui-round-more ui-set-absolute ui-set-c ui-fill-light-100 ui-bg-blur'>
+                                    <p className='ui-color-white ui-font-18 ui-m-15-b'>
+                                        <SvgIcon as='js' src={IconTrees} size='xl' />
+                                        <Spacer size={5} />
                                         {text?.forestImg}
                                     </p>
-                                    <Button block='sm' className='ui-round ui-theme-greenYellow ui-fill-dark-100'>
+                                    <Button block='sm' className='ui-round ui-fill-dark-100'>
                                         {text?.forestMore}
                                     </Button>
                                 </div>
@@ -75,7 +75,7 @@ export default function () {
                 <Grid.Col size={420} className='ui-p-10 ui-round-1st ui-theme-orangeRed'>
                     <Card className='ui-align-l ui-shadow-lg'>
                         <Card.Side className='ui-set-relative'>
-                            <span className='ui-color-white ui-p-10-v ui-p-20-h ui-m-30 ui-round ui-set-absolute ui-set-b ui-bg-blur'>
+                            <span className='ui-color-white ui-p-10-v ui-p-20-h ui-m-30 ui-round ui-set-absolute ui-set-b ui-fill-light-200 ui-bg-blur'>
                                 <SvgIcon as='js' src={IconTrophyStar} size='lg' r={10} />
                                 {text?.raceWinner}
                             </span>
@@ -108,35 +108,35 @@ export default function () {
                     </Card>
 
                     <Card className='ui-p-10 ui-shadow-lg'>
-                        <Card.Side className='ui-color-black-50 ui-m-10-b ui-theme-yellow ui-text'>
+                        <Card.Side className='ui-m-10-b'>
                             {text?.food}
                         </Card.Side>
                         <Card.Side>
                             <Button.Wrapper type='holder' as='div' ease='1st' className='ui-m-1-b ui-round-t-1st ui-block-2nd ui-icons-no-opacity'>
                                 <Button noease multi className='ui-p-10-v'>
-                                    <SvgIcon as='js' src={IconChef} b={5} className='ui-m-auto' />
+                                    <SvgIcon as='js' src={IconChef} b={5} className='ui-m-auto ui-text' />
                                     {text?.foodBtn1}
                                 </Button>
                                 <Button noease multi className='ui-p-10-v'>
-                                    <SvgIcon as='js' src={IconPlate} b={5} className='ui-m-auto' />
+                                    <SvgIcon as='js' src={IconPlate} b={5} className='ui-m-auto ui-text' />
                                     {text?.foodBtn2}
                                 </Button>
                                 <Button noease multi className='ui-p-10-v'>
-                                    <SvgIcon as='js' src={IconCup} b={5} className='ui-m-auto' />
+                                    <SvgIcon as='js' src={IconCup} b={5} className='ui-m-auto ui-text' />
                                     {text?.foodBtn3}
                                 </Button>
                             </Button.Wrapper>
                             <Button.Wrapper type='holder' as='div' ease='1st' className='ui-round-b-1st ui-block-2nd ui-icons-no-opacity'>
                                 <Button noease multi className='ui-p-10-v'>
-                                    <SvgIcon as='js' src={IconCoffee} b={5} className='ui-m-auto' />
+                                    <SvgIcon as='js' src={IconCoffee} b={5} className='ui-m-auto ui-text' />
                                     {text?.foodBtn4}
                                 </Button>
                                 <Button noease multi className='ui-p-10-v'>
-                                    <SvgIcon as='js' src={IconForkKnife} b={5} className='ui-m-auto' />
+                                    <SvgIcon as='js' src={IconForkKnife} b={5} className='ui-m-auto ui-text' />
                                     {text?.foodBtn5}
                                 </Button>
                                 <Button noease multi className='ui-p-10-v'>
-                                    <SvgIcon as='js' src={IconMitten} b={5} className='ui-m-auto' />
+                                    <SvgIcon as='js' src={IconMitten} b={5} className='ui-m-auto ui-text' />
                                     {text?.foodBtn6}
                                 </Button>
                             </Button.Wrapper>
@@ -149,69 +149,6 @@ export default function () {
         <Description>{desc?.sides}</Description>
         <Preview>
             <Grid.Row className='ui-align-l'>
-                <Grid.Col lg={3} size={6}>
-                    <Card className='ui-round ui-shadow'>
-                        <Card.Side className='ui-p-15'>
-                            <Heading as='h4'>{text?.ticketTitle1}</Heading>
-                        </Card.Side>
-                        <Card.Side className='ui-p-15-h'>
-                            {text?.ticket1}
-                        </Card.Side>
-                        <Card.Side className='ui-p-15'>
-                            <Heading as='h4'>{text?.ticketTitle2}</Heading>
-                        </Card.Side>
-                        <Card.Side className='ui-p-15-h'>
-                            {text?.ticket2}
-                        </Card.Side>
-                        <Card.Side className='ui-p-15'>
-                            <a href='#' className='ui-link ui-font-underline'>{text?.ticketUrl}</a>
-                        </Card.Side>
-                    </Card>
-                </Grid.Col>
-                <Grid.Col lg={3} size={6}>
-                    <Card className='ui-round ui-shadow ui-theme-gray'>
-                        <Card.Side className='ui-p-15'>
-                            <Heading as='h4'>{text?.ticketTitle1}</Heading>
-                        </Card.Side>
-                        <Card.Side className='ui-p-15 ui-fill-light-100'>
-                            {text?.ticket1}
-                        </Card.Side>
-                        <Card.Side className='ui-p-15'>
-                            <Heading as='h4'>{text?.ticketTitle2}</Heading>
-                        </Card.Side>
-                        <Card.Side className='ui-p-15 ui-fill-light-100'>
-                            {text?.ticket2}
-                        </Card.Side>
-                        <Card.Side className='ui-p-15'>
-                            <a href='#' className='ui-link ui-font-underline'>{text?.ticketUrl}</a>
-                        </Card.Side>
-                    </Card>
-                </Grid.Col>
-                <Grid.Col lg={3} size={6}>
-                    <Card className='ui-round ui-shadow ui-theme-yellowOrange'>
-                        <Card.Side className='ui-p-15 ui-fill-light-100 ui-text'>
-                            <Heading as='h4'>
-                                <SvgIcon as='js' src={IconTicket} />
-                                {text?.ticketTitle1}
-                            </Heading>
-                        </Card.Side>
-                        <Card.Side className='ui-p-15 ui-fill-light-200'>
-                            {text?.ticket1}
-                        </Card.Side>
-                        <Card.Side className='ui-p-15 ui-fill-light-100 ui-text'>
-                            <Heading as='h4'>
-                                <SvgIcon as='js' src={IconStore} />
-                                {text?.ticketTitle2}
-                            </Heading>
-                        </Card.Side>
-                        <Card.Side className='ui-p-15 ui-fill-light-200'>
-                            {text?.ticket2}
-                        </Card.Side>
-                        <Card.Side className='ui-p-15 ui-fill-light-100'>
-                            <a href='#' className='ui-link ui-font-underline'>{text?.ticketUrl}</a>
-                        </Card.Side>
-                    </Card>
-                </Grid.Col>
                 <Grid.Col lg={3} size={6}>
                     <Card className='ui-round ui-shadow ui-theme-yellowOrange'>
                         <Card.Side className='ui-p-15 ui-border-b'>
@@ -238,6 +175,123 @@ export default function () {
                             </Button>
                         </Card.Side>
                     </Card>
+                </Grid.Col>
+                <Grid.Col lg={3} size={6}>
+                    <Card className='ui-round ui-shadow ui-theme-gray'>
+                        <Card.Side className='ui-p-15 ui-fill-light-100'>
+                            <Heading as='h4' className='ui-theme-yellowOrange ui-text'>
+                                <SvgIcon as='js' src={IconTicket} />
+                                {text?.ticketTitle1}
+                            </Heading>
+                        </Card.Side>
+                        <Card.Side className='ui-p-15'>
+                            {text?.ticket1}
+                        </Card.Side>
+                        <Card.Side className='ui-p-15 ui-fill-light-100'>
+                            <Heading as='h4' className='ui-theme-yellowOrange ui-text'>
+                                <SvgIcon as='js' src={IconStore} />
+                                {text?.ticketTitle2}
+                            </Heading>
+                        </Card.Side>
+                        <Card.Side className='ui-p-15'>
+                            {text?.ticket2}
+                        </Card.Side>
+                        <Card.Side className='ui-p-15 ui-fill-light-100'>
+                            <a href='#' className='ui-link ui-font-underline'>{text?.ticketUrl}</a>
+                        </Card.Side>
+                    </Card>
+                </Grid.Col>
+                <Grid.Col lg={3} size={6}>
+                    <Card className='ui-round ui-shadow ui-theme-gray'>
+                        <Card.Side className='ui-p-15'>
+                            <Heading as='h4'>{text?.ticketTitle1}</Heading>
+                        </Card.Side>
+                        <Card.Side className='ui-p-15 ui-fill-light-100'>
+                            {text?.ticket1}
+                        </Card.Side>
+                        <Card.Side className='ui-p-15'>
+                            <Heading as='h4'>{text?.ticketTitle2}</Heading>
+                        </Card.Side>
+                        <Card.Side className='ui-p-15 ui-fill-light-100'>
+                            {text?.ticket2}
+                        </Card.Side>
+                        <Card.Side className='ui-p-15'>
+                            <a href='#' className='ui-link ui-font-underline'>{text?.ticketUrl}</a>
+                        </Card.Side>
+                    </Card>
+                </Grid.Col>
+                <Grid.Col lg={3} size={6}>
+                    <Card className='ui-round ui-shadow'>
+                        <Card.Side className='ui-p-15'>
+                            <Heading as='h4'>{text?.ticketTitle1}</Heading>
+                        </Card.Side>
+                        <Card.Side className='ui-p-15-h'>
+                            {text?.ticket1}
+                        </Card.Side>
+                        <Card.Side className='ui-p-15'>
+                            <Heading as='h4'>{text?.ticketTitle2}</Heading>
+                        </Card.Side>
+                        <Card.Side className='ui-p-15-h'>
+                            {text?.ticket2}
+                        </Card.Side>
+                        <Card.Side className='ui-p-15'>
+                            <a href='#' className='ui-link ui-font-underline'>{text?.ticketUrl}</a>
+                        </Card.Side>
+                    </Card>
+                </Grid.Col>
+            </Grid.Row>
+        </Preview>
+
+        <Description>{desc?.grids}</Description>
+        <Preview>
+            <Grid.Row>
+                <Grid.Col lg={6} size={12}>
+
+                    <Card className='ui-align-l ui-round ui-shadow-lg'>
+                        <Card.Side className='ui-p-15 ui-border-b'>
+                            <Heading as='h4'>{text?.week}</Heading>
+                        </Card.Side>
+                        <Card.Side>
+                            <Grid.Static fluid='sm'>
+                                <Grid.Col size={190} className='ui-no-p'>
+                                    <Image fluid src={img[2]} aspect='portrait' className='ui-hidden-sm ui-round-l ui-no-round-tl' />
+                                    <Image fluid src={img[2]} aspect='landscape' className='ui-visible-sm' />
+                                </Grid.Col>
+                                <Grid.Row gap='lg'>
+                                    <Grid.Col size={12}>
+                                        <Heading as='h3'>{text?.museumTitle}</Heading>
+                                        <p className='ui-font-readable ui-m-20-b'>{text?.museum}</p>
+                                        <Button.Wrapper as='span' ease='1st' className='ui-circle-1st'>
+                                            <Button noease as='span' size='xs' className='ui-p-10-h ui-m-5-r'>{text?.museumHash1}</Button>
+                                            <Button noease as='span' size='xs' className='ui-p-10-h'>{text?.museumHash2}</Button>
+                                        </Button.Wrapper>
+                                    </Grid.Col>
+                                </Grid.Row>
+                            </Grid.Static>
+                        </Card.Side>
+                    </Card>
+
+                </Grid.Col>
+                <Grid.Col lg={6} size={12}>
+
+                    <Card className='ui-round ui-shadow-lg'>
+                        <Grid.Row fluid='no' gap='xxs'>
+                            <Grid.Col size={4}>
+                                <Image fluid src={img[3]} aspect='square' className='ui-round-l ui-no-round-bl' />
+                            </Grid.Col>
+                            <Grid.Col size={4}>
+                                <Image fluid src={img[4]} aspect='square' />
+                            </Grid.Col>
+                            <Grid.Col size={4}>
+                                <Image fluid src={img[5]} aspect='square' className='ui-round-r ui-no-round-br' />
+                            </Grid.Col>
+                        </Grid.Row>
+                        <Grid.Row fluid='sm' gap='lg' className='ui-color-black-50 ui-font-readable'>
+                            <Grid.Col size={6}>{text?.travel1}</Grid.Col>
+                            <Grid.Col size={6}>{text?.travel2}</Grid.Col>
+                        </Grid.Row>
+                    </Card>
+
                 </Grid.Col>
             </Grid.Row>
         </Preview>
