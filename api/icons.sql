02fb8e97f91745b49dc7f8be8473e879322db274
aeminyuce <yuce_ahmet@hotmail.com>
2026-05-10 00:48:34 +0300

    update page and icons sqls

--- api/icons.sql
+++ api/icons.sql
@@ -4,27 +4,17 @@ LANGUAGE sql
 SECURITY DEFINER
 SET search_path = icons
 AS $$
-SELECT json_build_object(
-  'result', json_build_object(
-      'info', (
-        SELECT jsonb_strip_nulls(
-          jsonb_build_object(
-            'title', ici.title,
-            'image1', ici.image1,
-            'image2', ici.image2,
-            'text', ici.text,
-            'sizeChangerPrefix', ici.sizechangerprefix,
-            'sizeChangerSuffix', ici.sizechangersuffix,
-            'iconsSuffix', ici.iconssuffix
-          )
-        )
-        FROM icons.info ici
-        LIMIT 1
-      ),
-      'sizes', (
-        SELECT json_agg(to_jsonb(ics) - 'id' ORDER BY ics.id)
-        FROM icons.sizes ics
-      )
+SELECT jsonb_build_object(
+  'result', jsonb_build_object(
+    'info', (
+      SELECT to_jsonb(ici) - 'id'
+      FROM icons.info ici
+      LIMIT 1
+    ),
+    'sizes', (
+      SELECT json_agg(to_jsonb(ics) - 'id' ORDER BY ics.id)
+      FROM icons.sizes ics
     )
+  )
 );
 $$;
--- api/page.sql
+++ api/page.sql
@@ -4,27 +4,27 @@ LANGUAGE sql
 SECURITY DEFINER
 SET search_path = page
 AS $$
-SELECT json_build_object(
-  'result', json_build_object(
+SELECT jsonb_build_object(
+  'result', jsonb_build_object(
     'general', (
       SELECT to_jsonb(gnr) - 'id'
       FROM page.general gnr
       LIMIT 1
     ),
-    'header', json_strip_nulls(
-      json_build_object(
-        'getInTouch', (
-          SELECT to_jsonb(hgi) - 'id'
-          FROM page.header_getintouch hgi
-          LIMIT 1
-        ),
-        'headerLinks', (
-          SELECT json_agg(to_jsonb(hhl) - 'id' ORDER BY hhl.id)
-          FROM page.header_headerlinks hhl
-        ),
-        'socialLinks', (
-          SELECT json_agg(to_jsonb(hsl) - 'id' ORDER BY hsl.id)
-          FROM page.header_sociallinks hsl
+    'header', jsonb_strip_nulls(
+      jsonb_build_object(
+      'getInTouch', (
+        SELECT to_jsonb(hgi) - 'id'
+        FROM page.header_getintouch hgi
+        LIMIT 1
+      ),
+      'headerLinks', (
+        SELECT json_agg(to_jsonb(hhl) - 'id' ORDER BY hhl.id)
+        FROM page.header_headerlinks hhl
+      ),
+      'socialLinks', (
+        SELECT json_agg(to_jsonb(hsl) - 'id' ORDER BY hsl.id)
+        FROM page.header_sociallinks hsl
         )
       )
     )
