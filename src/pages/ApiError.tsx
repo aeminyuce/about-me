3ea5be8eb2f986d4bb90dde63ae19d2c2ca14f73
aeminyuce <yuce_ahmet@hotmail.com>
2026-05-30 01:46:50 +0300

    add home button

--- src/pages/ApiError.tsx
+++ src/pages/ApiError.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Button from 'uilab/react/Button';
 import Heading from 'uilab/react/Heading';
 
 export default function () {
@@ -7,9 +8,8 @@ export default function () {
             <Heading as='h1' className='ui-font-72'>
                 Error
             </Heading>
-            <p className='ui-font-22 ui-m-30-b'>
-                Whoops, the API paused or didn’t respond. We’ll be back soon.
-            </p>
+            <p className='ui-font-22 ui-m-30-b'>Whoops, the API paused or didn’t respond. We’ll be back soon.</p>
+            <Button className='ui-circle' to='/'>Go to Home</Button>
         </div>
     );
 }
