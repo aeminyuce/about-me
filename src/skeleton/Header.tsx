4ecb6c1dcb3677aeff5b0fb391074ff30151f308
aeminyuce <yuce_ahmet@hotmail.com>
2026-07-19 15:25:52 +0300

    tidy up

--- src/skeleton/Header.tsx
+++ src/skeleton/Header.tsx
@@ -6,14 +6,17 @@ export default function () {
     return (
         <Grid.Container as='div'>
             <Grid.Row fluid='no' className='ui-round-2nd'>
+
                 <Grid.Col size={9} md={3} sm={3} xs={3}>
                     <Skeleton repeat={5} w='92px' h='42px' hGap={2} className='ui-hidden-md' />
                     <Skeleton square h='42px' hGap={2} className='ui-visible-md' />
                 </Grid.Col>
+
                 <Grid.Col size={3} md={9} sm={9} xs={9} className='ui-align-r'>
                     <Skeleton square repeat={5} h='42px' hGap={1} />
                     <Skeleton square h='42px' hGap={2} className='ui-visible-md' />
                 </Grid.Col>
+
             </Grid.Row>
         </Grid.Container>
     )
