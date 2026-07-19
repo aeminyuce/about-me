e7566f6cae898db96235ad3884ff7c86b090086e
aeminyuce <yuce_ahmet@hotmail.com>
2026-07-19 17:56:50 +0300

    rearrange classname

--- src/components/home/MyFocus.tsx
+++ src/components/home/MyFocus.tsx
@@ -9,8 +9,8 @@ export default function () {
 
     return (
         <Grid.Row>
-            <Grid.Col size={8} push={2} className='ui-m-30-v'>
-                <p className='ui-font-lead ui-color-black-25 ui-font-readable ui-align-c'>
+            <Grid.Col size={8} push={2} className='ui-m-30-v ui-align-c'>
+                <p className='ui-font-lead ui-color-black-25 ui-font-readable'>
                     {api?.home?.aboutMe?.myFocus}
                 </p>
             </Grid.Col>
