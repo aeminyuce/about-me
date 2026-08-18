f5a910f27519b6f7ee56c46833ec0642c9460217
aeminyuce <yuce_ahmet@hotmail.com>
2026-08-18 22:04:33 +0300

    change prop

--- src/components/lab/BreadcrumbsWrapper.tsx
+++ src/components/lab/BreadcrumbsWrapper.tsx
@@ -8,10 +8,10 @@ export default function (props: BreadcrumbsWrapperProps) {
     const { children } = props;
 
     return (
-        <Breadcrumbs seoFriendly sep='bull' className='ui-p-10-b ui-m-30-b ui-border-b ui-border-light ui-ease-2nd-layout'>
-            <Breadcrumbs.Item seoFriendly pos={'1'} to='/' className={'ui-opacity'}>Home</Breadcrumbs.Item>
-            <Breadcrumbs.Item seoFriendly pos={'2'} to='/lab' className={'ui-opacity'}>Lab</Breadcrumbs.Item>
-            <Breadcrumbs.Item seoFriendly pos={'3'}>{children}</Breadcrumbs.Item>
+        <Breadcrumbs microdata sep='bull' className='ui-p-10-b ui-m-30-b ui-border-b ui-border-light ui-ease-2nd-layout'>
+            <Breadcrumbs.Item microdata pos='1' to='/' className='ui-opacity'>Home</Breadcrumbs.Item>
+            <Breadcrumbs.Item microdata pos='2' to='/lab' className='ui-opacity'>Lab</Breadcrumbs.Item>
+            <Breadcrumbs.Item microdata pos='3'>{children}</Breadcrumbs.Item>
         </Breadcrumbs>
     )
 }
